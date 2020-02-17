b27df5c6feb555a9d4ebae8f7cac05691ea0455f
jenishthanki <jenishhd@gmail.com>
2020-02-16 18:18:31 -0800

    Added Functionality

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "myDate.h"
 
+
 int main() {
 
-    myDate x(5,3,2);
-    x.display();
+    myDate x(3,6,1980);
+    string z = x.dayName();
+
+
+    cout<<z;
+
+
 
     return 0;
 }
--- myDate.cpp
+++ myDate.cpp
@@ -4,17 +4,26 @@ using namespace std;
 
 int Greg2Julian(int month, int day, int year){
     return (day-32075 + 1461*(year+4800+(month-14)/12)/4 + 367*(month-2-((month-14)/12)*12)/12 - 3*((year+4900+(month-14)/12)/100)/4);
-
 }
 
-void Julian2Greg(int JD, int& month, int& day, int &year){
-
+void Julian2Greg(int JD, int& m, int& d, int& y){
+    int L = JD + 68569;
+    int N = 4*L/146097;
+    L = L - (146097*N + 3)/4;
+    y = 4000 * (L + 1) / 1461001;
+    L = L - 1461 * y / 4 + 31;
+    m = 80 * L / 2447;
+    d = L - 2447 * m / 80;
+    L = m / 11;
+    m = m + 2 - 12 * L;
+    y = 100 * (N - 49) + y + L;
 }
 
-
-
-
-myDate:: myDate(){}
+myDate:: myDate(){
+    month = 5;
+    day = 11;
+    year = 1959;
+}
 
 myDate:: myDate(int m, int d, int y){
     month = m;
@@ -23,24 +32,86 @@ myDate:: myDate(int m, int d, int y){
 }
 
 void myDate:: display(){
-    cout<< month << day << year<<endl;
+    if(month == 1){ cout<< "January "<< day << ", " << year;}
+
+    else if(month == 2){ cout<< "Feburary "<< day << ", " << year;}
+    else if(month == 3){ cout<< "March "<< day << ", " << year;}
+    else if(month == 4){ cout<< "April "<< day << ", " << year;}
+    else if(month == 5){ cout<< "May "<< day << ", " << year;}
+    else if(month == 6){ cout<< "June "<< day << ", " << year;}
+    else if(month == 7){ cout<< "July "<< day << ", " << year;}
+    else if(month == 8){ cout<< "August "<< day << ", " << year;}
+    else if(month == 9){ cout<< "September " << day << ", " << year;}
+    else if(month == 10){ cout<< "October "<< day << ", " << year;}
+    else if(month == 11){ cout<< "November "<< day << ", " << year;}
+
+    else{ cout<< "December" << " " << day << ", " << year;}
+
+
+
 }
 
-void myDate:: increaseDate(int number){}
+void myDate:: increaseDate(int number){
+    int JD = Greg2Julian(month,day,year);
+    JD = JD + number;
+    Julian2Greg(JD,month,day,year);
+}
 
-void myDate:: decreaseDate(int number){}
+void myDate:: decreaseDate(int number){
+    int JD = Greg2Julian(month,day,year);
+    JD = JD - number;
+    Julian2Greg(JD,month,day,year);
+}
 
-int myDate:: daysBetween(myDate date){}
+int myDate:: daysBetween(myDate date){
+    int JD = Greg2Julian(month,day,year);
+    int givenDate = Greg2Julian(date.getMonth(),date.getDay(),date.getYear());
 
-int myDate:: getMonth(){}
+    return givenDate - JD;
 
-int myDate:: getDay(){}
+}
 
-int myDate:: getYear(){}
+int myDate:: dayOfYear(){ //Test case for 1st day should be zero
+    myDate x(1,1,year);
+    return -1*(daysBetween(x)-1);
+}
 
-int myDate:: dayOfYear(){}
+string myDate:: dayName() {
+    int JD = Greg2Julian(month,day,year);
+    if(JD%7 == 0){
+        return "Monday";
+    }
+    else if(JD%7 == 1){
+        return "Tuesday";
+    }
+    else if(JD%7 == 2){
+        return "Wednesday";
+    }
+    else if(JD%7 == 3){
+        return "Thursday";
+    }
+    else if(JD%7 == 4){
+        return "Friday";
+    }
+    else if(JD%7 == 5){
+        return "Saturday";
+    }
+    else
+        return "Sunday";
 
-string myDate:: dayName() {}
+}
+
+int myDate:: getMonth(){
+    return month;
+}
+
+int myDate:: getDay(){
+    return day;
+}
+
+int myDate:: getYear(){
+    return year;
+}
 
 
 
